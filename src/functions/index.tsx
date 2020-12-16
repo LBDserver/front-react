63e3774c78e29548dfe56af863462f96068f698c
jmauwerb <jeroen.werbrouck@ugent.be>
2020-12-16 19:23:37 +0100

    added separate file for LBDserver API connections

--- src/functions/index.tsx
+++ src/functions/index.tsx
@@ -1,21 +1,6 @@
-import { Project } from '@interfaces'
-import axios from 'axios'
-const {parse} = require('@frogcat/ttl2jsonld')
-
-// returns the projects with public accessibility stored locally in the server
-function getOpenProjects() {
-    return new Promise<Project[]>(async (resolve, reject) => {
-        try {
-            const {data} = await axios.get(`${process.env.REACT_APP_BACKEND}/lbd/public`)
-            data.projects.forEach((project: Project) => {
-                project["metadata"] = parse(project.metadata)
-            })
-            resolve(data.projects)
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
+import {
+    getOpenProjects
+} from './lbdApi'
 
 export {
     getOpenProjects
